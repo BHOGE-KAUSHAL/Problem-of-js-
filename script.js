306918e803481429b4cd87d47f6c3946c30c3c64
BHOGE-KAUSHAL <kaushalbhoge@gmail.com>
2025-11-16 16:03:34 +0530

    level-1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Level 1 
+// Level 1 Problems on js 
 //1. print number from 1 to 10 .
 //hint-loop from 1 to 10 and print each other
 
@@ -33,7 +33,7 @@ for(let j=0;j<5; j++ ){
 // for each number ,check:even -> "Even", else->"odd" 
 
 for(k=1;k<11;k++){
-    if(i % 2 ===0){
+    if(k % 2 ===0){
         console.log(`${k} Even`);
     }else{
         console.log(`${k} Odd`);
@@ -43,6 +43,7 @@ for(k=1;k<11;k++){
 // 6. ask user for a number say if it's positive or negative.
 // use prompt and conditional 
 // convert string into number // number(),parseInt(),+
+
 let num = +prompt("number batao");
 if (num >= 0){
     console.log("positive");
